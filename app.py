03d32f954c844cc7fa31d1e6f23bdefdd5695080
Yukidama <narururu19@gmail.com>
2021-08-06 22:18:08 +0900

    hello-heroku-flask

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello Heroku_Flask'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
