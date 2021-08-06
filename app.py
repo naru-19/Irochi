ba4c76a61eb36e307c2029dd084bd27623466524
Yukidama <narururu19@gmail.com>
2021-08-06 23:00:01 +0900

    update

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-
-
+UPLOAD_FOLDER = './static/images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.from_object(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     img_name = ""
     if request.method == 'POST':
-        stream = request.files['image'].stream
+        img_file = request.files['image']
         N_cols = request.form['name']
+        filename = secure_filename(img_file.filename)
+        img_url = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        img_file.save(img_url)
         print("img uploaded")
     if request.method == 'GET':
         print("get")
