4803181195cb6722c37d12a977b35111feb4ee97
Yukidama <narururu19@gmail.com>
2021-08-07 12:35:09 +0900

    update

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def rgbvalue(li, df):
     return rgbv
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
     img_name = ""
     error_flag=""
@@ -151,7 +151,7 @@ def index():
             if ori.width>ori.height:
                 ori=ori.resize((200,int(ori.height*200/ori.width)))    
             else:
-                ori=ori.resize((int(ori.width*200/ori.height),100))
+                ori=ori.resize((int(ori.width*200/ori.height),200))
 
             decopri("step1 画像を保存終了:"+str(time.time() - start))
             ori_ar=np.asarray(ori)[:,:,:3]
@@ -174,6 +174,64 @@ def index():
             data_df.to_csv(os.path.join(app.config['UPLOAD_FOLDER'] ,"data.csv"),index=False)
             hex_ori_df.to_csv(os.path.join(app.config['UPLOAD_FOLDER'] ,"original.csv"),index=False)
             decopri("step4 post終了:"+str(time.time() - start))
+    # if request.method == 'GET':
+    #     if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'] ,"data.csv")):
+    #         img_name = "ok"
+
+    #         start = time.time()
+
+    #         s=str(request.args.getlist('color'))
+    #         s=s[1:-1]
+    #         s=s.split(', ')
+
+    #         decopri("step1:"+str(time.time() - start))
+
+    #         for i in range(len(s)):
+    #             s[i]=s[i][1:-1]
+    #         col_li=s
+    #         data_df=pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'],"data.csv"))
+    #         hex_edit_df=pd.DataFrame(col_li).rename(columns={0:"color"})
+    #         hex_ori_df=pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'] ,"original.csv"))
+    #         edit_df=pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'],"img.csv"))
+
+    #         decopri("step2:"+str(time.time() - start))
+
+    #         for i in range(len(col_li)):
+    #             r,g,b=hex2rgb(col_li[i])
+    #             edit_df.loc[edit_df["group"]==i,"r"]=r
+    #             edit_df.loc[edit_df["group"]==i,"g"]=g
+    #             edit_df.loc[edit_df["group"]==i,"b"]=b
+    #         edit_df=edit_df.drop("group",axis=1)
+    #         ori_data[0] = hex_ori_df.columns 
+    #         ori_data[1] = hex_ori_df.values.tolist()
+    #         edit_data[0] = hex_edit_df.columns 
+    #         edit_data[1] = hex_edit_df.values.tolist()
+    #         N_cols=int(data_df.loc[0])
+    #         x=int(data_df.loc[1])
+    #         y=int(data_df.loc[2])
+    #         edit_img=np.array(edit_df)
+    #         edit_ar=edit_img.reshape([x,y,3])
+    #         print(edit_ar.shape)
+    #         edit_img = Image.fromarray(np.uint8(edit_ar))
+    #         isedit=True
+    #         edit_img.save(os.path.join(app.config['UPLOAD_FOLDER'],"edit.jpg"))
+    return render_template('index.html', img_name=img_name,
+        isedit=isedit, header_ori=ori_data[0], record_ori=ori_data[1],
+        N=N_cols,header_edit=edit_data[0], record_edit=edit_data[1],error_case=error_flag
+    )
+
+
+
+@app.route('/editer', methods=['GET'])
+def editer():
+    img_name = ""
+    error_flag=""
+    ori_data=["",""]
+    edit_data=["",""]
+    N_cols=""
+    isedit=""
+    cols=["r","g","b"]
+    data_img=[]
     if request.method == 'GET':
         if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'] ,"data.csv")):
             img_name = "ok"
@@ -219,9 +277,6 @@ def index():
         isedit=isedit, header_ori=ori_data[0], record_ori=ori_data[1],
         N=N_cols,header_edit=edit_data[0], record_edit=edit_data[1],error_case=error_flag
     )
-
-
-
 if __name__ == '__main__':
     app.debug = True
     app.run()
